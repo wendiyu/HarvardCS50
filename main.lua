a3d50a1f42658aa2e312a6953e2d602de4511c74
wendiyu <36240636+wendiyu@users.noreply.github.com>
2018-04-12 21:53:33 -0400

    add sound

--- main.lua
+++ main.lua
@@ -7,8 +7,15 @@
 -- two object meet together and make some sounds. Then move away.
 -----------------------------------------------------------------------------------------
 
+local soundEffect = audio.loadSound( "./assets/audio/hit.wav" )
+
+math.randomseed( os.time() )
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
+
+-- Start Physics
+ local physics = require("physics")
+ physics.start()
 
 -- local variables to this entire file
 local scrollSpeed = 5
@@ -22,21 +29,34 @@ backgroundImage.id = "winter background image"
 
 -- character image with width and height
 local NinjiaGirl = display.newImageRect("./assets/sprites/Ninjia.png", 500, 500)
-NinjiaGirl.x = 250
-NinjiaGirl.y = display.contentHeight - 200 -- note I am using not the center but the height of the diaplay
+NinjiaGirl.x = 50
+NinjiaGirl.y = display.contentHeight - 1100 -- note I am using not the center but the height of the diaplay
 NinjiaGirl.id = "Ninjia Girl"
 
 local Dog = display.newImageRect("./assets/sprites/Dog.png", 500, 500)
 Dog.x = 2048 
-Dog.y = display.contentHeight - 200 -- note I am using not the center but the height of the diaplay
+Dog.y = display.contentHeight - 320 -- note I am using not the center but the height of the diaplay
 Dog.id = "Dog"
 
 
 local function MoveImage(event)
 	-- add the scroll speed to the x-value of the image
-    NinjiaGirl.x = NinjiaGirl.x + scrollSpeed
-    Dog.x = Dog.x - scrollSpeed1
+	local randomX = math.random ( 1, 20 ) 
+	local randomY = math.random ( 0, 10 )
+	print(randomX, randomY)
+
+	NinjiaGirl.x = NinjiaGirl.x + randomX
+	NinjiaGirl.y = NinjiaGirl.y + randomY
+	Dog.x = Dog.x - scrollSpeed1
 end
 
 -- MoveNinjiaGirl will be called at program start over and over (60 frames or times per second)
 Runtime:addEventListener("enterFrame", MoveImage)
+
+
+audio.play( soundEffect )
+audio.setVolume( 0.5, { channel=1 } )
+
+if Dog.x == NinjiaGirl.x then
+	audio.play( soundEffect )
+end
